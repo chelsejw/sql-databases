d64cd4f787024e16ddcf72db24c68cbf0a014509
chelsejw <chelsejw@gmail.com>
2020-10-13 16:59:36 +0800

    add distinct

--- exercise.sql
+++ exercise.sql
@@ -336,3 +336,34 @@ select max(age(birth_date)) from employees;
 select count(id)
 from orders
 where extract(year from orderdate)='2004'AND extract(month from orderdate)='01';
+/*
+====================================================================
+DISTINCT
+=====================================================================
+*/
+
+/*
+* DB: Employees
+* Table: titles
+* Question: What unique titles do we have?
+*/
+
+select distinct title
+from titles;
+
+/*
+* DB: Employees
+* Table: employees
+* Question: How many unique birth dates are there?
+*/
+select count(distinct birth_date)
+from employees;
+/*
+* DB: World
+* Table: country
+* Question: Can I get a list of distinct life expectancy ages
+* Make sure there are no nulls
+*/
+select distinct lifeexpectancy
+from country
+where lifeexpectancy is not null;
