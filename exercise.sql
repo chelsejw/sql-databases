921c0df19e5bbece66cdc74440560aa3bceec377
chelsejw <chelsejw@gmail.com>
2020-10-13 15:47:55 +0800

    like operator

--- exercise.sql
+++ exercise.sql
@@ -222,3 +222,63 @@ where age between 30 and 50 AND income < 50000;
 select avg(income)
 from customers
 where age between 20 and 50; --59361.925908612832
+
+/*
+====================================================================
+LIKE AND ILIKE
+=====================================================================
+*/
+
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Find the age of all employees who's name starts with M.
+* Sample output: https://imgur.com/vXs4093
+* Use EXTRACT (YEAR FROM AGE(birth_date)) we will learn about this in later parts of the course
+*/
+select emp_no, first_name, EXTRACT (YEAR FROM AGE(birth_date)) as "age"
+from employees
+where first_name
+ilike 'g%';
+/*
+* DB: Employees
+* Table: employees
+* Question: How many people's name start with A and end with R?
+* Expected output: 1846
+*/
+select count(first_name)
+from employees
+where first_name
+ilike 'a%r';
+                                                  
+/*
+* DB: Store
+* Table: customers
+* Question: How many people's zipcode have a 2 in it?.
+* Expected output: 4211 
+*/
+
+select count(cast(zip as text))
+from customers
+where zip::text like '%2%';
+/*
+* DB: Store
+* Table: customers
+* Question: How many people's zipcode start with 2 with the 3rd character being a 1.
+* Expected output: 109 
+*/
+
+select count(cast(zip as text))
+from customers
+where zip::text like '2_1%';
+/*
+* DB: Store
+* Table: customers
+* Question: Which states have phone numbers starting with 302?
+* Replace null values with "No State"                                                  
+* Expected output: https://imgur.com/AVe6G4c
+*/
+select coalesce(state, 'No State'), phone
+from customers
+where phone like '302%';
